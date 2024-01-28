02dbd64f0f273a898d57666f1d42a9e810074cd4
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-01-28 22:25:10 +0530

    Create solution.py

---
+++ Leap Year | InterviewBit/solution.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def solve(self, A):
+        if A % 4 == 0:
+            if A % 100 == 0:
+                if A % 400 == 0:
+                    return 1
+                else:
+                    return 0
+            else:
+                return 1
+        else:
+            return 0  
