a9d84859f094987c39bff239eab30aebe595ef68
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-07-23 12:11:23 +0530

    Create solution.java

---
+++ Java Loops 1/solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+        bufferedReader.close();
+        
+        for (int i = 1; i <= 10; i++) {
+            int result = N * i;
+            System.out.println(N + " x " + i + " = " + result);
+        }
+    }
+}
