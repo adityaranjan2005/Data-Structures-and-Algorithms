a82e2f3ae0a6c46a13478f63c47372c9aa96466c
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-07-27 17:59:50 +0530

    Create solution.java

---
+++ End of File | Java/solution.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+class Solution
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int i = 1;
+        
+        while(sc.hasNext())
+        {
+            System.out.println(i + " " + sc.nextLine());
+            i++;
+        }
+        
+        sc.close();
+    }
+}
