1723f2655fb3b641c0a646cd5e70750af86199b7
Aditya Ranjan <145968304+adityaranjan2005@users.noreply.github.com>
2024-11-02 12:50:07 +0530

    Create solution.java

---
+++ Circular Sentence - Leetcode/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isCircularSentence(String sentence) {
+        int n = sentence.length();
+        if (sentence.charAt(0) != sentence.charAt(n-1)) return false;
+        for (int i = 1; i < n-1; i++) {
+            if (sentence.charAt(i) == ' ') {
+                if (sentence.charAt(i-1) != sentence.charAt(i+1)) return false;
+            }
+        }
+        return true;
+    }
+}
